ea3319c21eca9ba9a01c91d2ea65304daa0fa221
Marcellus Tavares <marcellustavares@gmail.com>
2017-09-18 10:21:07 -0300

    Add Form Samples portlet

---
+++ modules/form-samples-portlet/src/main/java/com/liferay/devcon/form/samples/portlet/FormSamplesPortlet.java
@@ -0,0 +1,26 @@
+package com.liferay.devcon.form.samples.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marcellus
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=form-samples-portlet Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class FormSamplesPortlet extends MVCPortlet {
+}
